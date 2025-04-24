ccedd9452a5544565b581358445eb9e90f06fc9c
SamiiBou <sami.boudechicha@gmail.com>
2025-04-24 18:59:41 +0200

    fix voucher

--- client/src/utils/encodeVoucher.ts
+++ client/src/utils/encodeVoucher.ts
@@ -1,10 +1,11 @@
-// utils/encodeVoucher.ts
-export function encodeVoucher(v: {
-    to: string;
-    amount: string;   // déjà stringifié
-    nonce: string;
-    deadline: string;
-  }) {
-    return [v.to, v.amount, v.nonce, v.deadline];
+export function encodeVoucher(v?: {
+  to: string;
+  amount: string;   // déjà stringifié
+  nonce: string;
+  deadline: string;
+}) {
+  if (!v) {
+    throw new Error("encodeVoucher → voucher manquant ou indéfini");
   }
-  
+  return [v.to, v.amount, v.nonce, v.deadline];
+}
