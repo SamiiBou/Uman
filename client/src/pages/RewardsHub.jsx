18b8f777a86c4f26c242d8412dc9f9fe82e167f9
Sami Boudechicha <sami.boudechicha@gmail.com>
2026-01-14 18:39:32 +0100

    increase participent number

--- client/src/pages/RewardsHub.jsx
+++ client/src/pages/RewardsHub.jsx
@@ -1106,7 +1106,7 @@ const RewardsHub = () => {
                 <Award size={24} />
               </div>
               <div className="challenge-badge limited">
-                <span>Limited to 300 first users!</span>
+                <span>Limited to 600 first users!</span>
               </div>
             </div>
 
@@ -1120,11 +1120,11 @@ const RewardsHub = () => {
                 <div className="counter-bar">
                   <div
                     className="counter-fill"
-                    style={{ width: `${(reviewChallengeStatus.participantCount / 300) * 100}%` }}
+                    style={{ width: `${(reviewChallengeStatus.participantCount / 600) * 100}%` }}
                   ></div>
                 </div>
                 <span className="counter-text">
-                  {reviewChallengeStatus.participantCount}/300 participants
+                  {reviewChallengeStatus.participantCount}/600 participants
                 </span>
               </div>
             </div>
