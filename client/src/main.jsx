7ef13f1d94efbe1236566b90e3f0deb4de168742
Sami Boudechicha <sami.boudechicha@gmail.com>
2026-04-08 12:48:09 +0100

    Remove eruda from client build

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import eruda from 'eruda';
 import App from './App'
 import './index.css'
 import { AuthProvider } from './context/AuthContext';
 import MiniKitProvider from "./minikit-provider";
-
-if (typeof window !== 'undefined') {
-  const erudaInstance = window.eruda || eruda;
-
-  if (!erudaInstance._isInit) {
-    erudaInstance.init();
-  }
-
-  erudaInstance.show();
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
